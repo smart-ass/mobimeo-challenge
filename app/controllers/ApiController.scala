b11946b227c680dcbdd95302ffad8936632d26e4
Kirill Veselov <kirill.veselov@springernature.com>
2018-09-16 16:21:43 +0200

    stub api endpoints

--- app/controllers/ApiController.scala
+++ app/controllers/ApiController.scala
@@ -1,10 +1,21 @@
 package controllers
 
 import javax.inject.{Inject, Singleton}
+import model.GetVehiclesRequest
+import play.api.Logger
+import play.api.libs.json.{JsObject, Json}
 import play.api.mvc.{AbstractController, ControllerComponents}
 
 @Singleton
 class ApiController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  def get = Action(Ok("ok"))
+  def getVehicles(getVehiclesRequest: GetVehiclesRequest) = Action {
+    Logger.debug(getVehiclesRequest.toString)
+    Ok(Json.toJson(JsObject.empty))
+  }
+
+  def isLineBusy(name: String) = Action {
+    Ok(Json.toJson(Json.obj("result" -> true, "line" -> name)))
+  }
+
 }
